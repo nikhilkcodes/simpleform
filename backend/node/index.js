1a020d0b12f01589db9ff5c9392416a1f91b3fa4
Nikhil Singh <86142267+nikhilkcodes@users.noreply.github.com>
2024-03-28 03:58:00 -0700

    UID and UDID Added

--- backend/node/index.js
+++ backend/node/index.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
-const cors = require('cors'); // Import the cors middleware
+const cors = require('cors');
+const { v4: uuidv4 } = require('uuid'); // Import uuidv4 from uuid library
 const app = express();
 
 app.use(bodyParser.json());
-app.use(cors()); // Use cors middleware to enable CORS
+app.use(cors());
 
 app.post('/submit-form', (req, res) => {
   const formData = req.body;
+
+  // Generate unique identifiers
+  const uniqueDeviceNumber = uuidv4();
+  const uniqueIdNumber = uuidv4();
+
+  // Add unique identifiers to form data
+  formData.uniqueDeviceNumber = uniqueDeviceNumber;
+  formData.uniqueIdNumber = uniqueIdNumber;
 
   // Log the form data to a file
   fs.appendFile('form-logs.txt', JSON.stringify(formData) + '\n', (err) => {
@@ -25,3 +34,4 @@ app.post('/submit-form', (req, res) => {
 app.listen(3001, () => {
   console.log('Server is running on port 3001');
 });
+
